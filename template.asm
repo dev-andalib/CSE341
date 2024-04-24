42c9ed2ea8be95ec83f21390119f28efeff21a24
Andalib Iftakher <iftakherandalib@gmail.com>
2024-04-24 09:51:59 +0600

    checking'

---
+++ template.asm
@@ -0,0 +1,25 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    ; DEFINE YOUR VARIABLES
+     
+   
+.CODE  
+    MAIN PROC
+        
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        ; YOUR CODE STARTS HERE 
+        
+       
+         
+
+        
+        ; YOUR CODE ENDS HERE
+        
+        MOV AX, 4C00H
+        INT 21H 
+        
+    MAIN ENDP
+    END MAIN                   
